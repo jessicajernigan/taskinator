5871b7761f07a40c8bd74387adc68556a57f8722
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-06-08 15:40:02 -0500

    end of 4.2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Create a list item, classed appropriately as a button, with specific text content.
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 // Upon click, create the list item described above.
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 

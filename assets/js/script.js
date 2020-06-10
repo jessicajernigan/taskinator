166c2ea291d7fb4de8e0bbe67cd43ad7b9e18786
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-06-10 18:30:02 -0500

    end of 4.3.11

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 
 
@@ -30,7 +32,7 @@ var taskFormHandler = function (event) {
   if (isEdit) {
     var taskId = formEl.getAttribute("data-task-id");
     completeEditTask(taskNameInput, taskTypeInput, taskId);
-  } 
+  }
   // no data attribute, so create object as normal and pass to createTaskEl function
   else {
     var taskDataObj = {
@@ -44,7 +46,17 @@ var taskFormHandler = function (event) {
 
 
 var completeEditTask = function (taskName, taskType, taskId) {
-  console.log(taskName, taskType, taskId);
+  // Find the matching task list item.
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  // Set new NAME and/or TYPE values.
+  taskSelected.querySelector("h3.task-name").textContent = taskName;
+  taskSelected.querySelector("span.task-type").textContent = taskType;
+
+  alert("Your task has been updated.");
+
+  formEl.removeAttribute("data-task-id");
+  document.querySelector("#save-task").textContent = "Add Task";
 };
 
 
@@ -55,7 +67,7 @@ var completeEditTask = function (taskName, taskType, taskId) {
 // • Create the 'div' and add HTML content (using innerHTML)
 // • Add list item to <ul> 
 
-var createTaskEL = function (taskDataObj) {
+var createTaskEl = function (taskDataObj) {
   // Create a list item.
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -177,14 +189,39 @@ var editTask = function (taskId) {
   document.querySelector("#save-task").textContent = "Save Task"; // Make it clear that the form is now in "edit mode" by updating the text of the submit button.
 
   formEl.setAttribute("data-task-id", taskId);
-}
+};
 
 // Function to DELETE a task.
 var deleteTask = function (taskId) {
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
   taskSelected.remove();
-}
+};
+
+
+// Function to MOVE TASK TO A NEW COLUMN.
+var taskStatusChangeHandler = function (event) {
+  // get the task item's id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  }
+  else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  }
+  else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+  }
+
+};
 
 
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
